3dd928b436d01d9f3748e64c4b515c534983a096
unknown <sayedhashim985@gmail.com>
2020-07-26 20:17:33 +0200

    s commit2

--- resources/views/admin/vendors/index.blade.php
+++ resources/views/admin/vendors/index.blade.php
@@ -9,7 +9,7 @@
                     <div class="row breadcrumbs-top">
                         <div class="breadcrumb-wrapper col-12">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">الرئيسية</a>
+                                <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">الرئيسيةة</a>
                                 </li>
                                 <li class="breadcrumb-item active"> ألمتاجر
                                 </li>
